6d1c8aeb1ddf2c444369c1fb4715a872a445f7d4
rm-reins <rm_reins@icloud.com>
2025-07-24 01:45:12 +0200

    feat: export components from index file & remove init component

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Tags } from "./Tags.vue";
+export { default as NoteCard } from "./NoteCard.vue";
+export { default as Search } from "./Search.vue";
+export { default as CreateEditModal } from "./CreateEditModal.vue";
